216e206e34f4cf3088afb9931941f8d14855b52a
shinokada <okada.shin@gmail.com>
2014-05-15 15:21:28 +0900

    Finish ruby/0017

--- ruby/0016.rb
+++ ruby/0016.rb
@@ -8,24 +8,15 @@ while n = gets.chomp
     p 'result is '
     result << 0
     result.flatten.each_slice(2){|arr| 
-      #distance = e[0].to_i  
-      #angle += e[1].to_i
-      #y << e[0].to_i
-      # x << e
       distance = arr[1].to_i
       angle += degtorad(arr[0].to_i)
       y << distance * Math.cos(angle)
       x << distance * Math.sin(angle)
-      #y << distance 
-      #x << angle
-   # end
     }
       final << x.inject(:+).to_i << y.inject(:+).to_i
-      #final << x << y
       p final
   else
     result << n.split(',')  
-#p result.flatten  
   end
 
   def degtorad(degree) 
---
+++ ruby/0017.rb
@@ -0,0 +1,22 @@
+input = 'xlmw mw xli tmgxyvi xlex m xsso mr xli xvmt.'
+output = 'this is the picture that i took in the trip.'
+
+ALPHBET = 'abcdefghihklmnopqrstuvwxyz'
+
+the = input.split.select{ |e| e.length == 3}
+alpha = ALPHBET.split('')
+result = []
+diff = alpha.index('t') - alpha.index(the[0][0])
+input_arr = input.split('')
+input_arr.map do |e|
+  if e == '.' 
+    result << '.'
+  elsif e == ' '
+    result << ' '
+  else
+    i = alpha.index(e).to_i + diff
+    result << alpha[i]
+  end
+end
+
+p result.join
